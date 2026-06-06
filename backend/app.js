eefcc86756d0fc41c600410b40851a6b9467ab38
banothsrikanth <banothsrikanth015@gmail.com>
2026-06-06 19:23:46 +0530

    frontend url update

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://invoice-management-dashboard-ten.vercel.app/",
     credentials: true,
   })
 );
